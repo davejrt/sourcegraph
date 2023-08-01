af3737bfff7177c9d96855f2990bc0f9034b8dba
William Bezuidenhout <william.bezuidenhout@sourcegraph.com>
2023-08-01 17:28:55 +0200

    move backcompat to bazel-backcompat.sh

---
+++ dev/ci/bazel-backcompat.sh
@@ -0,0 +1,22 @@
+
+set eu
+
+bazelrcs="--bazelrc=.bazelrc"
+
+if [[ "${CI:-false}" == "true" ]]; then
+  bazelrcs="${bazelrcs} --bazelrc=.aspect/bazelrc/ci.bazelrc --bazelrc=.aspect/bazelrc/ci.sourcegraph.bazelrc"
+fi
+
+echo "--- :git: generate mgirations diff and applying backcompat patch"
+git diff --ignore-space-at-eol v5.1.0..HEAD -- migrations/ > dev/backcompat/patches/back_compat_migrations.patch
+
+echo "--- :bazel: bazel test @sourcegraph_back_compat"
+bazel ${bazelrcs} \
+  test --test_tag_filters=go -- \
+  @sourcegraph_back_compat//cmd/... \
+  @sourcegraph_back_compat//lib/... \
+  @sourcegraph_back_compat//internal/... \
+  @sourcegraph_back_compat//enterprise/cmd/... \
+  @sourcegraph_back_compat//enterprise/internal/... \
+  -@sourcegraph_back_compat//cmd/migrator/... \
+  -@sourcegraph_back_compat//enterprise/cmd/migrator/...
