8ed0df50ff3f35eb99bd8ec0c6b5b8073c07afaf
William Bezuidenhout <william.bezuidenhout@sourcegraph.com>
2023-08-07 17:26:24 +0200

    force checkout

--- dev/ci/bazel-backcompat.sh
+++ dev/ci/bazel-backcompat.sh
@@ -7,11 +7,13 @@ if [[ "${CI:-false}" == "true" ]]; then
   bazelrcs="${bazelrcs} --bazelrc=.aspect/bazelrc/ci.bazelrc --bazelrc=.aspect/bazelrc/ci.sourcegraph.bazelrc"
 fi
 
+git diff
+
 echo "--- :git::rewind: checkout v5.1.0"
-git checkout "v5.1.0"
+git checkout --force "v5.1.0"
 
 echo "--- :git: generate mirations dtiff and applying backcompat patch"
-git checkout "${BUILDKITE_COMMIT}" -- migrations/
+git checkout --force "${BUILDKITE_COMMIT}" -- migrations/
 
 echo "--- :bazel: bazel test @sourcegraph_back_compat"
 bazel "${bazelrcs}" \
