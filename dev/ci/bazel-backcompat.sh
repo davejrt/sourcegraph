e922bab56b6a37cee3f6830272f836c76927c57f
William Bezuidenhout <william.bezuidenhout@sourcegraph.com>
2023-08-07 17:20:58 +0200

    git rewind

--- dev/ci/bazel-backcompat.sh
+++ dev/ci/bazel-backcompat.sh
@@ -1,20 +1,17 @@
 
 set eu
 
-echo "------------------------------------"
-target_commit=$(buildkite-agent metadata get "target_commit")
-echo "Target Commit: ${target_commit}"
-echo "Current Commit: $(git show HEAD)"
-echo "------------------------------------"
-
 bazelrcs="--bazelrc=.bazelrc"
 
 if [[ "${CI:-false}" == "true" ]]; then
   bazelrcs="${bazelrcs} --bazelrc=.aspect/bazelrc/ci.bazelrc --bazelrc=.aspect/bazelrc/ci.sourcegraph.bazelrc"
 fi
 
-echo "--- :git: generate mgirations dtiff and applying backcompat patch"
-git diff --ignore-space-at-eol "v5.1.0..${target_commit}" -- migrations/ > dev/backcompat/patches/back_compat_migrations.patch
+echo "--- :git::rewind: checkout v5.1.0"
+git checkout "v5.1.0"
+
+echo "--- :git: generate mirations dtiff and applying backcompat patch"
+git checkout "${BUILDKITE_COMMIT}" -- migrations/
 
 echo "--- :bazel: bazel test @sourcegraph_back_compat"
 bazel "${bazelrcs}" \
